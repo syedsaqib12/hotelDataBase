1d5b4c6085b0c35758c5270e9ec6dcd9ff265712
syedsaqib12 <49809570+syedsaqib12@users.noreply.github.com>
2019-07-13 06:00:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+Hotel 
+Name
+location
+dining area
+menu
+Summer offers
+staff
+Delivery
+Contact us
+
+*/
+let hotel = [
+         
+		 Name: "Royal Restaurant",
+		 location: "A-495 Allama Shabbir Ahmed Usmani Rd, Block 3 Gulshan-e-Iqbal, Karachi, Karachi City, Sindh",
+		 dining area: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+		 menu [
+		 {
+			 Burgers:["Scrappy Coco Burger","Danzinger","Fat Boy Slim Burger"],
+			 Sandwich: ["Kamikaze", "jjacjkajk", "sacjkakljajklc","ajksha"],
+			 
+			 
+			 
+			 }
+		 
+
+		 
