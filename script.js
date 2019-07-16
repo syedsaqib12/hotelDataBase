78914a9de55a1f8c52d4949e6d5f2b9bd4f0ade7
syedsaqib12 <49809570+syedsaqib12@users.noreply.github.com>
2019-07-17 02:55:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,25 +9,115 @@ staff
 Delivery
 Contact us
 
-function add(number1,number2) {
-return number1 + number2;
-add(8,2);
-}
-function multiply(numb1,numb2,numb3){
-    return numb1* numb2* numb3;
-}
-function multiplyManyNumbers(numbers) {
-    for (let i = 0; i < numbers.length)
-}
-// function ko input dene ke lye input me likhte hen or output kr lye curli breakit me retun likhte hen
-
 */
-let hotel = [
+ 
+ et hotel = [
          
 		 Name: "Royal Restaurant",
+		
 		 location: "A-495 Allama Shabbir Ahmed Usmani Rd, Block 3 Gulshan-e-Iqbal, Karachi, Karachi City, Sindh",
+		
 		 dining area: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
 		 menu [
 		 {
-			 Burgers:["Scrappy Coco Burger","Danzinger","Fat Boy Slim Burger"],
-			 Sandwich: ["Kamikaze", "jjacjkajk", "sacjkakljajklc","ajksha"],
+			 burgers:["Scrappy Coco Burger","Danzinger","Fat Boy Slim Burger"],
+			 
+			 sandwich: ["Kamikaze", "jjacjkajk", "sacjkakljajklc","ajksha"],
+			 
+			 desserts & Sweets: ["Mango Dessert", "Burmese Dessert","Zafrani Kaju ki Katli","Chocolate Lava Cake","Guilt-Free Dark Chocolate Mousse","Eggless Marble Cake","Nariyal Ladoo."]
+			 
+			 drinks: ["Bacardi Cocktail (rum)","Bahama Mama (rum)","Banana Cream Pie (RumChata)","Banana Daiquiri (rum)","Banana Split Martini (vodka)","Batida (cachaca)","Bay Breeze (vodka)"]
+			 
+			 summersOffer: ["Frankie and Bennys Vouchers EXCLUSIVE 25% Off Mains at Frankie & Benny's","Starter and Main for £11.95 at PizzaExpress","Kids Eat for £1 During the Summer Holidays at Harvester","Kids Eat for £1 During the Summer Holidays at Harvester","30% Off Food at Café Rouge"]
+			 
+			 staff: [
+			 
+			   {
+        name: "saqib",
+        age: 24,
+		designation:"Manager"
+        nic: 16736 - 17813 - 7617 - 8,
+        height: 400,
+        contactNo: 0881251519179,
+    },
+{
+        name: "aleem",
+        age: 22,
+        nic: 16736 - 17813 - 7617 - 8,
+		designation:"Supervisor"
+        height: 400,
+        contactNo: 09912300179,
+    },
+{
+        name: "kashif",
+        age: 27,
+        nic: 16736 - 17813 - 7617 - 8,
+        designation:"Accountant"
+		height: 500,
+        contactNo: 07712389179,
+    },
+{
+        name: "ahmed",
+        age: 20,
+        nic: 26736 - 17813 - 7617 - 8,
+		designation:"Waiter"
+        height: 600,
+        contactNo: 05512389179,
+    }
+	{
+        name: "nazim",
+        age: 20,
+        nic: 26736 - 17813 - 7617 - 8,
+		designation:"chief"
+        height: 900,
+        contactNo: 05512389179,
+    }
+	{
+        name: "salman",
+        age: 28,
+        nic: 26736 - 17813 - 7617 - 8,
+		designation:"Cleaner"
+        height: 600,
+        contactNo: 05512389179,
+    }
+	{
+        name: "neha",
+        age: 17,
+        nic: 27736 - 17813 - 7617 - 8,
+		designation:"Marketing Agent"
+        height: 60,
+        contactNo: 05512389179,
+    }
+	{
+        name: "ayesha",
+        age: 23,
+        nic: 28736 - 17813 - 7617 - 8,
+		designation:"Receptionist"
+        height: 200,
+        contactNo: 05512389179,
+    }
+			 
+			 ]
+			 
+			 delievery: ["1. Decision Convenience – Making it fast and easy to choose. For eg. on Zomato and Food Panda, you can view ratings and reviews of restaurants, and decide where to order from accordingly," 
+			 
+"2. Access Convenience – Making it fast and easy to acquire. For eg. Hot Meals are delivered to you at your location within 15 minutes using SpoonRocket, making it easy to eat well wherever and whenever you are hungry.",
+
+"3.Transaction Convenience – Making it fast and easy to pay. For eg. All apps let you pay with cards, PayPal accounts or wallets like Apple Pay.",
+
+"4.Benefit Convenience – Making it fast and easy to enjoy/use. For eg. On Postmates, you can ask for delivery from more than one restaurant, and get it straight to your home.",
+
+"5.Post-Benefit Convenience – Making it fast and easy to re-purchase. For eg. You can save your address and payment details, bookmark your favorites to re-order in seconds."]
+
+         contactUs: ["If you have an immediate issue please contact us by calling from 7am – 11 pm CST", "Badar Commercial Street 10 Phase V, Badar Commercial Area Phase 5 Defence Housing Authority Karachi 
+
+Phone: 021111112522
+
+Mon, Tues, Wed, Thur, Fri, Sat, Sun"]
+			 
+			 
+			 
+			 }
+		 
+
+		 
